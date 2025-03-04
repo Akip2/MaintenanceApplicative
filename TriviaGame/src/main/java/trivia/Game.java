812a03a89d28c45294c096f13287efa04fbbe64b
Corentin <corentin.froger@netc.fr>
2025-03-04 11:22:09 +0100

    Refactor handleCorrectAnswer(), suppression code réutilisé dans la méthode

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -109,27 +109,19 @@ public class Game implements IGame {
 
     public boolean handleCorrectAnswer() {
         if (inPenaltyBox[currentPlayer]) {
-            if (isGettingOutOfPenaltyBox) {
-                System.out.println("Answer was correct!!!!");
-                purses[currentPlayer]++;
-                System.out.println(players.get(currentPlayer) + " now has " + purses[currentPlayer] + " Gold Coins.");
-
-                boolean winner = didPlayerWin();
-                nextPlayer();
-                return winner;
-            } else {
+            if (!isGettingOutOfPenaltyBox) {
                 nextPlayer();
                 return true;
             }
-        } else {
-            System.out.println("Answer was correct!!!!");
-            purses[currentPlayer]++;
-            System.out.println(players.get(currentPlayer) + " now has " + purses[currentPlayer] + " Gold Coins.");
-
-            boolean winner = didPlayerWin();
-            nextPlayer();
-            return winner;
         }
+
+        System.out.println("Answer was correct!!!!");
+        purses[currentPlayer]++;
+        System.out.println(players.get(currentPlayer) + " now has " + purses[currentPlayer] + " Gold Coins.");
+
+        boolean winner = didPlayerWin();
+        nextPlayer();
+        return winner;
     }
 
     public boolean wrongAnswer() {
